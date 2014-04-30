b78644d61c60a6f50e5aa5385fd539e8abb9d6d7
Justin <justin@objectrocket.com>
2014-04-30 12:27:00 -0500

    Test for delete_acl

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,12 @@ Make sure that qatools is imported before app is initialized, otherwise the
 app will attempt to make DB connections before a DB is in place.
 """
 import sys
+
 import pytest
+
+from viper import config
+from viper.instance import InstanceManager
+
 
 def pytest_addoption(parser):
     parser.addoption("--testdb", action="store_true", default=False,
@@ -24,3 +29,9 @@ def app_client(testdb):
     if 'app' not in sys.modules:
         from gui import app
     return app.test_client()
+
+
+def get_instance(login, instance):
+    instance_manager = InstanceManager(config)
+    instance = instance_manager.get_instance_by_name(login, instance)
+    return instance
