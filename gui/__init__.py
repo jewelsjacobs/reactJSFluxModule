54b7683d7fc8b5372350606da3745d8e00fab069
Anthony Dodd <Dodd.AnthonyJosiah@gmail.com>
2014-05-27 15:40:25 -0500

    Using multiple configuration objects for GUI.
    It would be great to throw the current views into a blueprint, but that
    will take a bit more work.

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,14 +1,27 @@
 """App initialization."""
-from flask import Flask, abort
+from flask import Flask
 
-from viper import config
+from gui.config import config_map
+from viper import config as viper_config
 
-from http_exceptions import PaymentRequired
 
-# Initialize the app.
-abort.mapping[402] = PaymentRequired
-app = Flask(__name__)
-app.config.from_object(config)
+# TODO(Anthony): if we use blueprints, then we can simply register the blueprint
+# below and then we won't have to check for VIPER_IN_DEV.
+def create_app(config_name='default'):
+    """Application factory."""
+    # Construct application and initialize configuration.
+    app = Flask(__name__)
+    config = config_map[config_name]()  # Instantiate config.
+    app.config.from_object(config)
+    config.init_app(app)
+
+    return app
+
+# Expose an instance of app for production gunicorn script.
+if viper_config.VIPER_IN_DEV:
+    app = create_app('development')
+else:
+    app = create_app('production')
 
 # Load views.
 from gui import views  # noqa
