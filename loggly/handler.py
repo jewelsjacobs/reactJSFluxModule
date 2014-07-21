f63e10ee376454e1138068a0864baf168b61e7f2
Justin <justin@objectrocket.com>
2014-07-21 09:56:51 -0500

    Fixes 406. Adds loggly support.

---
+++ loggly/handler.py
@@ -0,0 +1,32 @@
+import requests
+
+from logging import Handler
+
+
+class LogglyHandler(Handler):
+    def __init__(self, token, tags=None):
+        """Logging handler for Loggly
+
+        :param token: str
+        :param tags: list or tuple
+        :return:
+        """
+        super(LogglyHandler, self).__init__()
+
+        if tags:
+            if not isinstance(tags, (list, tuple)):
+                raise TypeError("Keyword argument 'tags' must be a list or tuple.")
+            tag_str = 'tag/{}/'.format(','.join(tags))
+        else:
+            tag_str = ''
+
+        self.url = 'https://logs-01.loggly.com/inputs/{}/{}'.format(token, tag_str)
+        print(self.url)
+
+    def emit(self, record):
+        """Emit log record
+
+        :param record: str
+        :return:
+        """
+        requests.post(self.url, data=self.format(record), headers={'content-type': 'text/plain'}, timeout=10)
