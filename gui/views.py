a3a0c1d6913babc6643489cc445aa81e3adbcede
Troy Swanson <gerphimum@gmail.com>
2014-07-29 15:27:53 -0500

    Remove routes for old sign up process

--- gui/views.py
+++ gui/views.py
@@ -1301,27 +1301,6 @@ def sign_up():
     return render_template('sign_up/sign_up.html')
 
 
-@app.route('/sign_up1', methods=['GET', 'POST'])
-def sign_up1():
-    """Deprecated route to sign up and create a user account."""
-    return redirect(url_for('sign_up'))
-
-
-@app.route('/sign_up2', methods=['GET', 'POST'])
-def sign_up2():
-    return redirect(url_for('sign_up'))
-
-
-@app.route('/sign_up3', methods=['GET', 'POST'])
-def sign_up3():
-    return redirect(url_for('sign_up'))
-
-
-@app.route('/sign_up_finish', methods=['GET', 'POST'])
-def sign_up_finish():
-    return redirect(url_for('sign_up'))
-
-
 @app.route('/msa')
 @viper_auth
 def msa():
