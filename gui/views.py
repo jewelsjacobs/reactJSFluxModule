6bae263271e84a75dd185e98a5fa3edb6b273887
John Moore <melderan@gmail.com>
2014-12-26 13:04:25 -0600

    Capture new read only field from add user form and pass to user creation method

--- gui/views.py
+++ gui/views.py
@@ -756,10 +756,11 @@ def add_instance_user(selected_instance):
 
     user = request.form['username']
     password = request.form['password']
+    readonly = request.form.get('readonly', 'off') == 'on'
 
     for database in instance.databases:
         try:
-            instance.add_user(database.name, user, password)
+            instance.add_user(database.name, user, password, readonly)
         except Exception as ex:
             exception_uuid = Utility.obfuscate_exception_message(ex.message)
             flash_message = ("There was a problem updating user information for instance %s. If "
@@ -796,10 +797,11 @@ def create_instance_user(selected_instance, selected_database=None):
 
             user = request.form['username']
             password = request.form['password']
+            readonly = request.form.get('readonly', 'off') == 'on'
 
             if user_instance.has_database(selected_database):
                 # The database exists, just add a user to it.
-                user_instance.add_user(selected_database, user, password)
+                user_instance.add_user(selected_database, user, password, readonly)
 
                 # Redirect to database because that's where this request came from.
                 # See refactor note above.
