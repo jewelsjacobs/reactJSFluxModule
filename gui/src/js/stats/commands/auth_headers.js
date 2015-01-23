322ead358ec13beb0f550167d5f25e090e7b93b5
Julia Jacobs <jjacobs2000@me.com>
2015-01-23 15:49:14 -0600

    dont ^%0 ask

--- gui/src/js/stats/commands/auth_headers.js
+++ gui/src/js/stats/commands/auth_headers.js
@@ -34,15 +34,10 @@ AuthHeadersCommand.prototype = _.extend({}, BaseCommand.prototype, {
         // make the request for the api urls
         request.get(TOKEN_ROUTE)
             .end(function (err, response) {
-                //_authHeaders = {
-                //  "X-Auth-Account": response.body['user'],
-                //  "X-Auth-Token": response.body['api_token']
-                //};
-
-               _authHeaders = {
-                 "X-Auth-Account": "appboy",
-                 "X-Auth-Token": "IjM4MjRlYzI5YWVjOTQwZDNhZGU3NmQwNjExMDJiN2VhIg.B6RLMQ.IqG2XqvuDr2Qdh-5o84e2Ab12Vg"
-               };
+                _authHeaders = {
+                  "X-Auth-Account": response.body['user'],
+                  "X-Auth-Token": response.body['api_token']
+                };
 
                 callback(err, _authHeaders);
         });
